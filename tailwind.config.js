3aee255db4ff1d7dcdf9b97e8f7a8dca74df54e4
mizaeldragon <raidechin@gmail.com>
2024-11-22 14:12:04 -0300

    feat: add font poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
